d09edf8611fd23f3fe8ab65e25387039af4059c4
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-21 16:45:48 -0500

    created data for skeleton function tests

---
+++ src/data.js
@@ -0,0 +1,54 @@
+const DATA = [
+  {
+    instruction: 'Write a function called skeletonFuncOne that creates the skeleton of a function',
+    answer: function skeletonFuncOne () {
+    }
+  },
+  {
+    instruction: 'Write a function called skeletonFuncTwo that creates the skeleton of a function',
+    answer: function skeletonFuncTwo () {
+    }
+  },
+  {
+    instruction: 'Write a function called skeletonFuncThree that creates the skeleton of a function',
+    answer: function skeletonFuncThree () {
+    }
+  },
+  {
+    instruction: 'Write a function called skeletonFuncFour that creates the skeleton of a function',
+    answer: function skeletonFuncFour () {
+    }
+  },
+  {
+    instruction: 'Write a function called skeletonFuncFive that creates the skeleton of a function',
+    answer: function skeletonFuncFive () {
+    }
+  },
+  {
+    instruction: 'Write a function called skeletonFuncSix that creates the skeleton of a function',
+    answer: function skeletonFuncSix () {
+    }
+  },
+  {
+    instruction: 'Write a function called skeletonFuncSeven that creates the skeleton of a function',
+    answer: function skeletonFuncSeven () {
+    }
+  },
+  {
+    instruction: 'Write a function called skeletonFuncEight that creates the skeleton of a function',
+    answer: function skeletonFuncEight () {
+    }
+  },
+  {
+    instruction: 'Write a function called skeletonFuncNine that creates the skeleton of a function',
+    answer: function skeletonFuncNine () {
+    }
+  },
+  {
+    instruction: 'Write a function called skeletonFuncTen that creates the skeleton of a function',
+    answer: function skeletonFuncTen () {
+    }
+  }
+]
+
+export default DATA
