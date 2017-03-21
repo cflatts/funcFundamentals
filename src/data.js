e310b4256e616866b8c1f8ab87e9df5e387c0d04
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-21 16:55:04 -0500

    created data for giving input to a function

--- src/data.js
+++ src/data.js
@@ -48,6 +48,66 @@ const DATA = [
     instruction: 'Write a function called skeletonFuncTen that creates the skeleton of a function',
     answer: function skeletonFuncTen () {
     }
+  },
+  {
+    instruction: 'Write a function called inputFuncOne that has an parameter called inputOne',
+    answer: function inputFuncOne (inputOne) {
+
+    }
+  },
+  {
+    instruction: 'Write a function called inputFuncTwo that has an parameter called inputTwo',
+    answer: function inputFuncTwo (inputTwo) {
+
+    }
+  },
+  {
+    instruction: 'Write a function called inputFuncThree that has an parameter called inputThree',
+    answer: function inputFuncThree (inputThree) {
+
+    }
+  },
+  {
+    instruction: 'Write a function called inputFuncFour that has an parameter called inputFour',
+    answer: function inputFuncFour (inputFour) {
+
+    }
+  },
+  {
+    instruction: 'Write a function called inputFuncFive that has an parameter called inputFive',
+    answer: function inputFuncFive (inputFive) {
+
+    }
+  },
+  {
+    instruction: 'Write a function called inputFuncSix that has an parameter called inputSix',
+    answer: function inputFuncSix (inputSix) {
+
+    }
+  },
+  {
+    instruction: 'Write a function called inputFuncSeven that has an parameter called inputSeven',
+    answer: function inputFuncSeven (inputSeven) {
+
+    }
+  },
+  {
+    instruction: 'Write a function called inputFuncEight that has an parameter called inputEight',
+    answer: function inputFuncEight (inputEight) {
+
+    }
+  },
+  {
+    instruction: 'Write a function called inputFuncNine that has an parameter called inputNine',
+    answer: function inputFuncNine (inputNine) {
+
+    }
+  },
+  {
+    instruction: 'Write a function called inputFuncTen that has an parameter called inputTen',
+    answer: function inputFuncTen (inputTen) {
+
+    }
   }
 ]
 
